5f7403a5a4d790587cb6869b726229df8e797f57
Clifton Ferguson <clifton.ferguson7@gmail.com>
2023-07-25 14:40:38 -0500

    added fields and constructor

---
+++ src/VehicleApp/Vehicle.java
@@ -0,0 +1,16 @@
+package VehicleApp;
+
+public class Vehicle {
+private String name;
+private int currentSpeed;
+private int currentDirection;
+public Vehicle(String name) {
+	super();
+	this.name = name;
+	this.currentSpeed = 0;
+	this.currentDirection = 0;
+}
+
+
+	
+}
