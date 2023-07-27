9a5908421b908d04ab9c2a5eade3991eb6ca370a
Clifton Ferguson <clifton.ferguson7@gmail.com>
2023-07-27 08:23:32 -0500

    built Vehicle class

--- src/VehicleApp/Vehicle.java
+++ src/VehicleApp/Vehicle.java
@@ -18,6 +18,36 @@ public void steer(int direction) {
 	public void move(int speed, int direction) {
 		currentSpeed = speed;
 		currentDirection = direction;
-	System.out.println("move method called: Moving at "+currentSpeed+ " in direction" + currentDirection);
+	System.out.println("move method called: Moving at "+currentSpeed+ " in direction " + currentDirection);
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCurrentSpeed() {
+		return currentSpeed;
+	}
+
+	public void setCurrentSpeed(int currentSpeed) {
+		this.currentSpeed = currentSpeed;
+	}
+
+	public int getCurrentDirection() {
+		return currentDirection;
+	}
+
+	public void setCurrentDirection(int currentDirection) {
+		this.currentDirection = currentDirection;
+	}
+	
+	public void stop() {
+		this.currentSpeed = 0;
+	}
+	
+	
 }
